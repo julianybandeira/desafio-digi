c472b2ad5f321db17d7d69bc76001716bbf22c5f
Juliany Bandeira <juliany12121989@gmail.com>
2022-05-20 15:35:45 -0300

    terceiro desafio

---
+++ desafio-boas-vindas/script.js
@@ -0,0 +1,6 @@
+function infoUser(){
+    let nameUser = prompt('Qual é o seu nome?')
+    let ageUser = prompt(`Olá, ${nameUser}! Quantos anos você tem?`)
+    
+    return alert(`Acabei de conhecer ${nameUser}, que tem ${ageUser} anos de idade!`)
+}
